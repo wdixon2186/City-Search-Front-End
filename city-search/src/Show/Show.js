e5d8f6e1b34a5e3f2d9567bd8afab95ceef8c397
Jared Morgan <jared@majordanger.com>
2019-10-21 17:55:13 -0500

    debugging Show still in progress, pushing to switch drivers post break

--- city-search/src/Show/Show.js
+++ city-search/src/Show/Show.js
@@ -1,18 +1,45 @@
 import React, { Component } from "react";
+import listOfCities from "../cities-data.json";
 import Comment from "../Comment/Comment";
 
 export default class List extends Component {
   render() {
+    let cityPop = listOfCities[0].population;
+    // console.log(cityPop);
+
+    let showCity = "Austin"; // this.props.match.params.city
+    let city = listOfCities.filter(city => city.general.city === showCity);
+    let population = city[0].population;
+
+    console.log(city);
+    console.log(population);
+
+    // let population = city.populations
+
+    let populationDisplay = city.map(data => {
+      return (
+        <div>
+          <p>
+            <li>{data}</li>
+          </p>
+        </div>
+      );
+    });
+
     return (
       <div>
-        <h2>City</h2>
-        <h3>State</h3>
+        <h2>{listOfCities[0].general.city}</h2>
+        <h3>{listOfCities[0].general.stateFull}</h3>
         <div>
           <h4>Population</h4>
           <ul>
-            <li>Pop 2018: 1,000,000</li>
-            <li>Pop 2010: 500,000</li>
-            <li>Change: 100%</li>
+            {/* {population} */}
+            {/* <li>
+              {Object.keys(listOfCities[0].population)}:{" "}
+              {listOfCities[0].population.popEst2018}
+            </li>
+            <li>Change: {listOfCities[0].population.popChange}</li>
+            <li>Pop 2010: {listOfCities[0].population.Pop2010}</li> */}
           </ul>
           <Comment />
         </div>
