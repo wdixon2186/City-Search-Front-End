b529548c6326318755b39d6bd99c370fbd871f08
Jared Morgan <jared@majordanger.com>
2019-10-24 08:59:02 -0500

    Updates

---
+++ precache-manifest.b9663a9d9fc18428a6baeb713aecc1f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d15b8f762396af465825558de72f5db",
+    "url": "/City-Search-Front-End/index.html"
+  },
+  {
+    "revision": "28fcc6515af7303f1da5",
+    "url": "/City-Search-Front-End/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "546c5d61fd6f87ac250c",
+    "url": "/City-Search-Front-End/static/css/main.4fdb73c4.chunk.css"
+  },
+  {
+    "revision": "28fcc6515af7303f1da5",
+    "url": "/City-Search-Front-End/static/js/2.a7d5cdad.chunk.js"
+  },
+  {
+    "revision": "546c5d61fd6f87ac250c",
+    "url": "/City-Search-Front-End/static/js/main.1f1a50f5.chunk.js"
+  },
+  {
+    "revision": "58bd7a3053d8a782ef94",
+    "url": "/City-Search-Front-End/static/js/runtime-main.49f143d0.js"
+  },
+  {
+    "revision": "f88c0938dcbd9c22d7e58bb4f5a188cb",
+    "url": "/City-Search-Front-End/static/media/city-fyndr-logo.f88c0938.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/City-Search-Front-End/precache-manifest.b9663a9d9fc18428a6baeb713aecc1f8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/City-Search-Front-End/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
